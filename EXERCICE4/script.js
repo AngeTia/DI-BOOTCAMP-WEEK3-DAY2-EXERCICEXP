312f3714d826199071c9f2f903a2a38552f9e6c8
AngeTia <tiaange03@gmail.com>
2023-01-06 17:54:14 +0000

    Mise à jour de mon Exercice

--- EXERCICE4/script.js
+++ EXERCICE4/script.js
@@ -1,13 +1,13 @@
 // Define a function to calculate the volume of a sphere
 function calculateVolume(radius) {
     // Calculate the volume using the formula: 4/3 * pi * r^3
-    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    let volume = (4/3) * Math.PI * Math.pow(radius, 3);
     return volume;
 }
 
 // Get a reference to the form and submit button
-var form = document.getElementById('MyForm');
-var submitButton = document.getElementById('submit');
+let form = document.getElementById('MyForm');
+let submitButton = document.getElementById('submit');
 
 // Add a submit event listener to the form
 form.addEventListener('submit', function(event) {
@@ -15,10 +15,10 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Get the radius value from the form
-    var radius = document.getElementById('radius').value;
+    let radius = document.getElementById('radius').value;
 
     // Calculate the volume of the sphere
-    var volume = calculateVolume(radius);
+    let volume = calculateVolume(radius);
 
     // Display the volume in the volume field
     document.getElementById('volume').value = volume;
